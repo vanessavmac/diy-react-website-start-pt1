56c5acd8e801a74243fac9f90fc9c1a12467357b
Vanessa Mac <vanessavaleriemac@gmail.com>
2022-08-18 23:57:28 -0400

    Initial commit

---
+++ part-2-start/src/components/AboutMe.js
@@ -0,0 +1,14 @@
+function AboutMe() {
+  return (
+    <div>
+      {/* Style react component using inline JSX */}
+      <h1 style={{ margin: '0px' }}>hi, i'm vanessa</h1>
+      <p>
+        I am a 2nd year biomedical engineering student with a passion for
+        building accessible websites.
+      </p>
+    </div>
+  );
+}
+
+export default AboutMe;
